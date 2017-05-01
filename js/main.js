fd0889c36d25be379e4d412655c88a7aaf39b773
Humphrey Obuobi <hobuobi@Humphreys-MacBook-Air.local>
2017-05-01 16:36:47 -0400

    design overhaul

--- js/main.js
+++ js/main.js
@@ -1,5 +1,49 @@
+function moveRight(){
+    $("#main").animate({
+        left: "-50%"
+    },300)
+}
+function moveLeft(){
+    $("#main").animate({
+        left: "0%"
+    },300)
+}
+function whereInject(){
+    $("#choice-action").append("<h1 class='text-upper'>Pick a place.</h1><h2>Choose from any of the following, then press CONTINUE.</h2><input name='place-search' id='place-search' type='text' placeholder='Search a place here.'><br><div class='place choice' id='lamont'><p class='text-upper text-red'>Lamont Library</p><p>Capacity: 80 People</p><p><span class='text-green text-upper'>OPEN</span> | Open from 8AM to 12AM</p></div><div class='place choice' id='md-lobby'><p class='text-upper text-red'>Maxwell Dworkin Lobby</p><p>Capacity: 30 People</p><p><span class='text-green text-upper'>OPEN</span> | Open from 8AM to 12AM</p></div><div class='place choice' id='lev'><p class='text-upper text-red'>Leverett Dining Hall</p><p>Capacity: 150 People</p><p><span class='text-green text-upper'>OPEN</span> | Open from 8AM to 12AM</p></div>");
+}
+function whenInject(){
+    $("#choice-action").append("<h1 class='text-upper'>Pick a time.</h1><h2>Pick a time by adjusting the sliders below.</h2><br><div id='when-box'><div class='half'><div id='when-slider'></div></div><div class='half'><div class='when-preselect' id='morning'><img src='img/morning.png'></div><div class='when-preselect' id='afternoon'><img src='img/afternoon.png'></div><div class='when-preselect' id='evening'><img src='img/evening.png'></div></div></div>")
+    var whenSlider = document.getElementById('when-slider');
+        whenSlider.style.height = '400px';
+        whenSlider.style.margin = '0 auto 30px';
+        noUiSlider.create(whenSlider, {
+            behaviour: 'tap-drag',
+            start: [ 600, 1200 ],
+            tooltips: [true,true],
+            connect: [false,true,false],
+            range: {
+                'min': [  600 ],
+                'max': [ 2300 ]
+            },
+            step: 100,
+            orientation: 'vertical'
+        });
+}
+
 $(document).ready(function(){
-    $("#where").click(function(){
-        $("#where-select").fadeIn(300);
+    $(".choice").click(function(){
+        $(this).siblings().removeClass("active")
+        $(this).addClass("active");
     })
+    $(".pref").click(function(){
+        $("#choice-action").empty();
+        if($(this).attr('id') == 'where'){
+            whereInject();
+        }
+        else{
+            whenInject();
+        }
+        moveRight()
+    })
+    $("#back").click(moveLeft);
 })
